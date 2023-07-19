dc5837f9c8e7306c5071f147d7d5f8ea38cf1f6e
rnezil <rylandnezil@gmail.com>
2023-07-18 17:12:51 -0700

    Adds pop member function 	pop removes and item from the queue
    	pop is thread safe
    
    Adds close member function
    	close changes queue status from open to closed
    
    	close is thread safe
    
    Adds clear member function
    	clear removes all items from queue
    
    	clear is thread safe
    
    Adds is_full member function
    	is_full returns true if queue size equals queue maximum
    	size
    
    	is_full is not thread safe
    
    Adds is_empty member function
    	is_empty returns true if queue size equals zero
    
    	is_empty is not thread safe
    
    Adds is_closed member function
    	is_closed returns true if queue status is closed
    
    	is_closed is not thread safe
    
    Adds max_size member function
    	max_size returns maximum allowed number of items in the
    	queue
    
    	max_size is not thread safe

--- include/ra/queue.hpp
+++ include/ra/queue.hpp
@@ -27,8 +27,7 @@ public:
 
 	queue() = delete;
 
-	queue( size_type max_size ): storage {},
-		size {0}, capacity {max_size}, closed {false} {}
+	queue( size_type max_size ): storage {}, capacity {max_size}, closed {false} {}
 
 	queue( const queue& ) = delete;
 	queue& operator=( const queue& ) = delete;
@@ -44,44 +43,114 @@ public:
 	}
 
 	status push( value_type&& x ){
-		// Take the mutex
-		std::unique_lock halt(qutex);
+		if( is_closed() ){
+			return status::closed;
+		}else{
+			// Take the mutex
+			std::unique_lock halt(qutex);
 
-		// Sleep the thread until there is room to push
-		// something into the queue
-		full_q.wait(halt, [this](){return size < capacity;});
+			// Sleep the thread until there is room to push
+			// something into the queue
+			pushreq.wait(halt, [this](){return is_closed() || !is_full();});
 
-		// Do the operation
-		storage.emplace(x);
+			if( is_closed() ){
+				return status::closed;
+			}else{
+				// Do the operation
+				storage.emplace(x);
+				
+				// Notify popper
+				popreq.notify_one();
 
-		// Update size
-		++size;
-
-		return status::success;
+				return status::success;
+			}
+		}
 	}
 
-	status pop( value_type& x );
+	status pop( value_type& x ){
+		if( is_closed() ){
+			if( is_empty() ){
+				return status::closed;
+			}else{
+				// Grab the mutex
+				std::unique_lock halt(qutex);
 
-	void close();
+				// Place value in x
+				x = storage.front();
 
-	void clear();
+				// Pop it
+				storage.pop();
 
-	bool is_full() const;
+				// Notify pusher
+				pushreq.notify_one();
 
-	bool is_empty() const;
+				return status::success;
+			}
+		}else{
+			// Grab the mutex
+			std::unique_lock halt(qutex);
 
-	bool is_closed() const;
+			// Wait until there is something to pop
+			popreq.wait(halt, [this](){return !is_empty();});
 
-	size_type max_size() const;
+			// Storage value in x
+			x = storage.front();
+
+			// Pop it
+			storage.pop();
+
+			// Notify pusher
+			pushreq.notify_one();
+
+			return status::success;
+		}
+	}
+
+	void close(){
+		if( !is_closed() ){
+			// Grab the mutex
+			std::unique_lock halt(qutex);
+
+			// Close the queue
+			closed = true;
+		}
+	}
+
+	void clear(){
+		// Grab the mutex
+		std::unique_lock halt(qutex);
+
+		// Record size before clearing
+		int size = storage.size();
+
+		// Clear the queue
+		while( !storage.empty )
+			storage.pop();
+
+		// Notify pusher(s)
+		while( size != 0 ){
+			pushreq.notify_one();
+			--size;
+		}
+	}
+
+
+
+	bool is_full() const; { return storage.size() == capacity; }
+
+	bool is_empty() const { return storage.size() == 0; }
+
+	bool is_closed() const { return closed; }
+
+	size_type max_size() const { return capacity; }
 
 private:
-	std::mutex qutex;
-	std::condition_variable full_q;
-	std::condition_variable empty_q;
+	mutable std::mutex qutex;
+	mutable std::condition_variable pushreq;
+	mutable std::condition_variable popreq;
 	std::queue<value_type> storage;
 	bool closed;
-	size_type size;
-	size_type capacity;
+	const size_type capacity;
 };
 
 }
