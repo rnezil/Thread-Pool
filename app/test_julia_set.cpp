a950c1e2130b51a0b7237fb6abf37438d8d65e7e
rnezil <rylandnezil@gmail.com>
2023-07-27 10:38:53 -0700

    Adds more test code for thread pool class

--- app/test_julia_set.cpp
+++ app/test_julia_set.cpp
@@ -10,15 +10,15 @@ int main() {
 	int H = 512;
 	int sample = 255;
 	boost::multi_array<int, 2> a( boost::extents[H][W] );
-	/*ra::fractal::compute_julia_set( bot, top, c, sample, a, 4 );
+/*	ra::fractal::compute_julia_set( bot, top, c, sample, a, 4 );
 	std::cout << "P2 " << W << ' ' << H << ' ' << sample << '\n';
 	for( int m = H; m; --m ){
 		for( int n = 0; n < W; ++n ){
 			std::cout << a[m-1][n] << ' ';
 		}
 		std::cout << '\n';
-	}*/
-
+	}
+*/
 
 
 	boost::multi_array<int, 2> one( boost::extents[H][W] );
--- app/test_thread_pool.cpp
+++ app/test_thread_pool.cpp
@@ -3,139 +3,138 @@
 #include "ra/thread_pool.hpp"
 #include <iostream>
 #include <cassert>
+#include <set>
 #include <catch2/catch.hpp>
-
-/*
- * NOTE TO THE MARKER:
- *
- * It is expected that not ALL test cases
- * will be passed in this program. That is
- * due to mutex acquisition not guaranteeing
- * FIFO order, i.e. if several threads are
- * trying to grab the mutex then there is no
- * guarantee that the first thread to try 
- * will be the first thread to get it once
- * it becomes available. Therefore, when
- * there is enough pressure on the queue,
- * jobs can be scheduled in an order that
- * does not correspond to the order in which
- * they were scheduled in the main thread.
- *
- * This can be observed directly by taking
- * a closer look at the test cases that fail:
- * they are always due to the ordering of two
- * scheduled operations being swapped from
- * what one would expect if the operations
- * had been carried out sequentially vs being
- * scheduled into the thread pool. Thus, in
- * these cases, two threads were both waiting
- * on the mutex.
- *
- */
 
 using namespace ra::concurrency;
 
 TEST_CASE( "Test thread pool with default construction", "[thread_pool]" ){
 	thread_pool tp;
-	queue<int> numbers(33000);
-	int vals[32768];
-	int* trans = &vals[0];
+	queue<int> numbers(4100);
+	std::set<int> trans {};
+	CHECK( trans.empty() );
+	int tmp;
+	std::mutex m;
 	CHECK( numbers.is_empty() );
-	for( int i = 0; i < 32768; ++i ){
+	for( int i = 0; i < 4096; ++i ){
 		tp.schedule( [&numbers, i](){int j = i; numbers.push(std::move(j));} );
 	}
-	for( int i = 0; i < 32768; ++i ){
-		tp.schedule( [&numbers, trans, i](){numbers.pop(*(trans+i));} );
+	for( int i = 0; i < 4096; ++i ){
+		tp.schedule( [&numbers, &trans, i, &tmp, &m](){
+				numbers.pop(tmp);
+				std::unique_lock lock(m);
+				trans.insert(tmp);
+				} );
 	}
 	tp.shutdown();
-	for( int i = 0; i < 32768; ++i )
-		CHECK( *(trans+i) == i );
+	for( int i = 0; i < 4096; ++i )
+		CHECK( trans.contains(i) );
 	CHECK( tp.is_shutdown() );
 	CHECK( numbers.is_empty() );
 	thread_pool t1;
-	queue<int> number1(60000);
-	int val1[59049];
-	int* tran1 = &val1[0];
+	queue<int> number1(2200);
+	trans.clear();
+	CHECK( trans.empty() );
 	CHECK( number1.is_empty() );
-	for( int i = 0; i < 59049; ++i ){
+	for( int i = 0; i < 2187; ++i ){
 		t1.schedule( [&number1, i](){int j = i; number1.push(std::move(j));} );
 	}
-	for( int i = 0; i < 59049; ++i ){
-		t1.schedule( [&number1, tran1, i](){number1.pop(*(tran1+i));} );
+	for( int i = 0; i < 2187; ++i ){
+		t1.schedule( [&number1, &trans, i, &tmp, &m](){number1.pop(tmp);
+				std::unique_lock lock(m);
+				trans.insert(tmp);
+				} );
 	}
 	t1.shutdown();
-	for( int i = 0; i < 59049; ++i )
-		CHECK( *(tran1+i) == i );
+	for( int i = 0; i < 2187; ++i )
+		CHECK( trans.contains(i) );
 	CHECK( t1.is_shutdown() );
 	CHECK( number1.is_empty() );
 }
 
 TEST_CASE( "Test thread pool with parametrized construction", "[thread_pool]" ){
 	thread_pool tp(128);
-	queue<int> numbers(33000);
-	int vals[32768];
-	int* trans = &vals[0];
+	queue<int> numbers(4100);
+	std::set<int> trans {};
+	CHECK( trans.empty() );
+	int tmp;
+	std::mutex m;
 	CHECK( numbers.is_empty() );
-	for( int i = 0; i < 32768; ++i ){
+	for( int i = 0; i < 4096; ++i ){
 		tp.schedule( [&numbers, i](){int j = i; numbers.push(std::move(j));} );
 	}
-	for( int i = 0; i < 32768; ++i ){
-		tp.schedule( [&numbers, trans, i](){numbers.pop(*(trans+i));} );
+	for( int i = 0; i < 4096; ++i ){
+		tp.schedule( [&numbers, &trans, i, &tmp, &m](){
+				numbers.pop(tmp);
+				std::unique_lock lock(m);
+				trans.insert(tmp);} );
 	}
 	tp.shutdown();
-	for( int i = 0; i < 32768; ++i )
-		CHECK( *(trans+i) == i );
+	for( int i = 0; i < 4096; ++i )
+		CHECK( trans.contains(i) );
 	CHECK( tp.is_shutdown() );
 	CHECK( numbers.is_empty() );
 	thread_pool t1(7);
-	queue<int> number1(60000);
-	int val1[59049];
-	int* tran1 = &val1[0];
+	queue<int> number1(2200);
+	trans.clear();
+	CHECK( trans.empty() );
 	CHECK( number1.is_empty() );
-	for( int i = 0; i < 59049; ++i ){
+	for( int i = 0; i < 2187; ++i ){
 		t1.schedule( [&number1, i](){int j = i; number1.push(std::move(j));} );
 	}
-	for( int i = 0; i < 59049; ++i ){
-		t1.schedule( [&number1, tran1, i](){number1.pop(*(tran1+i));} );
+	for( int i = 0; i < 2187; ++i ){
+		t1.schedule( [&number1, &trans, i, &tmp, &m](){
+				number1.pop(tmp);
+				std::unique_lock lock(m);
+				trans.insert(tmp);} );
 	}
 	t1.shutdown();
-	for( int i = 0; i < 59049; ++i )
-		CHECK( *(tran1+i) == i );
+	for( int i = 0; i < 2187; ++i )
+		CHECK( trans.contains(i) );
 	CHECK( t1.is_shutdown() );
 	CHECK( number1.is_empty() );
 }
 
 TEST_CASE( "Test thread pool further with parametrized construction", "[thread_pool]" ){
 	thread_pool tp(50);
-	queue<int> numbers(33000);
-	int vals[32768];
-	int* trans = &vals[0];
+	queue<int> numbers(4100);
+	std::set<int> trans {};
+	CHECK( trans.empty() );
+	int tmp;
+	std::mutex m;
 	CHECK( numbers.is_empty() );
-	for( int i = 0; i < 32768; ++i ){
+	for( int i = 0; i < 4096; ++i ){
 		tp.schedule( [&numbers, i](){int j = i; numbers.push(std::move(j));} );
 	}
-	for( int i = 0; i < 32768; ++i ){
-		tp.schedule( [&numbers, trans, i](){numbers.pop(*(trans+i));} );
+	for( int i = 0; i < 4096; ++i ){
+		tp.schedule( [&numbers, &trans, i, &tmp, &m](){
+				numbers.pop(tmp);
+				std::unique_lock lock(m);
+				trans.insert(tmp);
+				} );
 	}
 	tp.shutdown();
-	for( int i = 0; i < 32768; ++i )
-		CHECK( *(trans+i) == i );
+	for( int i = 0; i < 4096; ++i )
+		CHECK( trans.contains(i) );
 	CHECK( tp.is_shutdown() );
 	CHECK( numbers.is_empty() );
 	thread_pool t1(5);
-	queue<int> number1(60000);
-	int val1[59049];
-	int* tran1 = &val1[0];
+	queue<int> number1(2200);
+	trans.clear();
+	CHECK( trans.empty() );
 	CHECK( number1.is_empty() );
-	for( int i = 0; i < 59049; ++i ){
+	for( int i = 0; i < 2187; ++i ){
 		t1.schedule( [&number1, i](){int j = i; number1.push(std::move(j));} );
 	}
-	for( int i = 0; i < 59049; ++i ){
-		t1.schedule( [&number1, tran1, i](){number1.pop(*(tran1+i));} );
+	for( int i = 0; i < 2187; ++i ){
+		t1.schedule( [&number1, &trans, i, &tmp, &m](){
+				number1.pop(tmp);
+				std::unique_lock lock(m);
+				trans.insert(tmp);} );
 	}
 	t1.shutdown();
-	for( int i = 0; i < 59049; ++i )
-		CHECK( *(tran1+i) == i );
+	for( int i = 0; i < 2187; ++i )
+		CHECK( trans.contains(i) );
 	CHECK( t1.is_shutdown() );
 	CHECK( number1.is_empty() );
 }
